ec30ae4b19fa73c43427accacd4f8678ce4894e8
ilerrr <116286032+ilerrr@users.noreply.github.com>
2025-02-07 11:36:32 +0300

    Add files via upload

---
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace Calculator;
+
+class Program
+{
+    static void Main()
+    {
+        int firstNumber = 10;
+        int secondNumber = 5;
+        int totalSum = AddNumbers(firstNumber, secondNumber);
+
+        Console.WriteLine($"Сумма: {totalSum}");
+        Console.ReadLine();
+    }
+
+    static int AddNumbers(int x, int y)
+    {
+        return x + y;
+    }
+}
